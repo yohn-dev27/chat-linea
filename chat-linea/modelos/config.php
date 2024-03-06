76628cb4e4bca968b41eedf1b80f662b2db7c5fe
Yohn Baldovino <162075048+yohn-dev27@users.noreply.github.com>
2024-03-06 14:15:08 -0500

    Add files via upload

---
+++ chat-linea/modelos/config.php
@@ -0,0 +1,6 @@
+<?php
+
+define('URL_BASE', 'http://127.0.0.1/chat_online/');
+define('SOCKET_FRONTEND', '127.0.0.1:80');
+define('SOCKET_BACKEND_IP', '127.0.0.1');
+define('SOCKET_BACKEND_PORT', '80');
